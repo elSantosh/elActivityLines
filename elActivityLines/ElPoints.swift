c85ede4c53408437632c39b3be4262adedefb0b5
Santosh Guruju  | MACROKIOSK <santosh.guruju@macrokiosk.com>
2017-05-10 17:30:07 +0800

    Initial commit

---
+++ elActivityLines/ElPoints.swift
@@ -0,0 +1,42 @@
+//
+//  ElPoints.swift
+//  elActivityLines
+//
+//  Created by Santosh Guruju  | MACROKIOSK on 10/05/2017.
+//  Copyright © 2017 workstreak. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+open class ISPoint {
+    
+    open var title:String
+    open var description:String?
+    open var pointColor:UIColor
+    open var lineColor:UIColor
+    open var touchUpInside:Optional<(_ point:ISPoint) -> Void>
+    open var fill:Bool
+    
+    public init(title:String, description:String, pointColor:UIColor, lineColor:UIColor, touchUpInside:Optional<(_ point:ISPoint) -> Void>, fill:Bool) {
+        self.title = title
+        self.description = description
+        self.pointColor = pointColor
+        self.lineColor = lineColor
+        self.touchUpInside = touchUpInside
+        self.fill = fill
+    }
+    
+    public convenience init(title:String, description:String, touchUpInside:Optional<(_ point:ISPoint) -> Void>) {
+        let defaultColor = UIColor.init(red: 0.75, green: 0.75, blue: 0.75, alpha: 1.0)
+        self.init(title: title, description: description, pointColor: defaultColor, lineColor: defaultColor, touchUpInside: touchUpInside, fill: false)
+    }
+    
+    public convenience init(title:String, touchUpInside:Optional<(_ point:ISPoint) -> Void>) {
+        self.init(title: title, description: "", touchUpInside: touchUpInside)
+    }
+    
+    public convenience init(title:String) {
+        self.init(title: title, touchUpInside: nil)
+    }
+}
--- elActivityLines/ViewController.swift
+++ elActivityLines/ViewController.swift
@@ -12,7 +12,34 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let frame = CGRect(origin: CGPoint(x: 10,y :20), size: CGSize(width: 300, height: 600))
+        let timeline = ISTimeline(frame: frame)
+        timeline.backgroundColor = .clear
+        timeline.bubbleColor = .green
+        let point = ISPoint(title: "Activity One")
+        
+        point.description = "Sample Description"
+        point.lineColor = .red
+        point.fill = true
+        point.pointColor = .darkText
+        self.view.addSubview(timeline)
+        
+        let point2 = ISPoint(title: "Activity Two")
+        point2.description = "Sample Description"
+        point2.lineColor = .red
+        point2.fill = true
+        point2.pointColor = .darkText
+        
+        let myPoints = [
+            ISPoint(title: "first set node"),
+            ISPoint(title: "second set node"),
+            ISPoint(title: "third set node")
+        ]
+        timeline.points = [point,point2]
+        timeline.points.append(contentsOf: myPoints)
+        
+
     }
 
     override func didReceiveMemoryWarning() {
